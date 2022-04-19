d9cd5d2293afd72884e7c66431058780a1c00c85
wfgm5k2d <nktpgrlv@gmail.com>
2022-04-19 22:07:31 +0700

    add content in template create.blade.php

--- html/resources/views/create.blade.php
+++ html/resources/views/create.blade.php
@@ -1,1 +1,23 @@
-<?php
+@extends('layouts.app')
+
+@section('title')
+    Создание заметки
+@endsection
+
+@section('content')
+
+    <form action="" method="post" class="mt-4 p-4">
+        <div class="form-group m-3">
+            <label for="name">Название</label>
+            <input type="text" class="form-control" name="name">
+        </div>
+        <div class="form-group m-3">
+            <label for="description">Описание</label>
+            <textarea class="form-control" name="description" rows="3"></textarea>
+        </div>
+        <div class="form-group m-3">
+            <input type="submit" class="btn btn-primary float-end" value="Создать">
+        </div>
+    </form>
+
+@endsection
